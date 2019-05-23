83f29c72701b04b77506d0c8a64773cba993b590
Julius Dehner <julius.dehner@gmail.com>
2019-05-23 13:09:05 +0200

    svn functions

---
+++ .config/fish/functions/svnfina.fish
@@ -0,0 +1,9 @@
+function svnfina
+	cd ~/Projects/finakom_root/finakom/trunk
+svn update
+cd ~/Projects/finakom_root/finakom_druck/trunk
+svn update
+cd ~/Projects/finakom_root/finakom_dummy/trunk
+svn update
+cd
+end
---
+++ .config/fish/functions/svnvera.fish
@@ -0,0 +1,9 @@
+function svnvera
+	cd ~/Projects/veraneo/trunk/veraneo
+svn update
+cd ~/Projects/veraneo/trunk/veraneo-dummyservice
+svn update
+cd ~/Projects/veraneo/trunk/veraneo-druckservice
+svn update
+cd
+end
