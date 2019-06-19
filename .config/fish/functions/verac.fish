8561ec62787927845006d8137a287ca341da5b5b
Julius Dehner <julius.dehner@gmail.com>
2019-06-19 14:49:29 +0200

    update stuff

--- .config/fish/functions/verac.fish
+++ .config/fish/functions/verac.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function verac --description 'alias verac cd ~/Projects/veraneo/trunk/veraneo/client/'
-	cd ~/Projects/veraneo/trunk/veraneo/client/ $argv;
+	cd /home/julius/Projects/veraneo/veraneo/client $argv;
 end
