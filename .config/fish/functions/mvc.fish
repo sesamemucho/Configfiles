74fa1b098e9cda6228f8cec64bad73b73e362e46
Julius Dehner <julius.dehner@gmail.com>
2019-08-20 12:41:10 +0200

    mvc command

---
+++ .config/fish/functions/mvc.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function mvc --description 'alias mvc cd ~/Projects/mv/Kundennavigator-2/veraneo/trunk/client/'
+	cd ~/Projects/mv/Kundennavigator-2/veraneo/trunk/client/ $argv;
+end
