3856ba2b3a44217888ea6fdb50ce112bc4205ba8
Julius Dehner <julius.dehner@gmail.com>
2019-10-30 16:51:04 +0100

    fix startup.sh

--- .config/i3/startup.sh
+++ .config/i3/startup.sh
@@ -4,12 +4,12 @@ VBoxClient-all &
 compton &
 xbanish &
 
-exec_always --no-startup-id ~/.config/i3/polybar.sh &
+~/.config/i3/polybar.sh &
 
 #Xresources
-exec_always xrdb ~/.Xresources
-exec dunst
-exec feh --randomize --bg-fill --no-xinerama ~/Pictures/*
-exec --no-startup-id lightscreen -d
-exec --no-startup-id redshift-gtk -l 48.458510:11.832830 -t 6500K:3800K
+xrdb ~/.Xresources
+dunst
+feh --randomize --bg-fill --no-xinerama ~/Pictures/*
+--no-startup-id lightscreen -d
+--no-startup-id redshift-gtk -l 48.458510:11.832830 -t 6500K:3800K
 
