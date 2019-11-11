9e3f47ecc962dfd98560a4798c335f8661918476
Julius Dehner <julius.dehner@gmail.com>
2019-11-11 18:18:22 +0100

    some nice packages :3

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,20 +22,3 @@
 (setq custom-file "~/.emacs.d/customfile.el")
 (load custom-file)
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (flatland-theme)))
- '(custom-safe-themes
-   (quote
-    ("9b35c097a5025d5da1c97dba45fed027e4fb92faecbd2f89c2a79d2d80975181" "dcf7154867ba67b250fe2c5cdc15a7d170acd9cbe6707cc36d9dd1462282224d")))
- '(inhibit-startup-screen t)
- '(org-agenda-files
-   (quote
-    ("~/.emacs.d/config.org" "~/Notebooks/org/gcal.org" "~/Notebooks/org/i.org")))
- '(package-selected-packages
-   (quote
-    (exwm-edit dumb-jump exwm-systemtray exwm xref-js2 js2-refactor js2-mode emmet-mode polymode aggressive-indent agressive-indent atomic-chrome counsel-projectile ace-window ob-kotlin kotlin-mode flatland-theme projectile yasnippet htmlize vterm org-gcal spaceline rainbow-delimiters sudo-edit hungry-delete rainbow-mode rg fzf memoize diff-hl org-bullets which-key  magit deferred company   adoc-mode dashboard all-the-icons page-break-lines ranger ))))
