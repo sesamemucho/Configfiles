30a06eb92bd85d0858267affaf4f99ba02f14f5e
Julius Dehner <julius.dehner@gmail.com>
2019-11-22 20:33:37 +0100

    fix exwm focus follows mouse

---
+++ .config/fish/functions/maggot.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function maggot --description alias\ maggot\ wine\ /hdd/Visual\\\ Novels/Maggot\\\ Baits/maggotbaits.exe
+	wine /hdd/Visual\ Novels/Maggot\ Baits/maggotbaits.exe $argv;
+end
