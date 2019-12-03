a80f3198c2ae46647efdeceeffc66b7c5af496c2
Julius Dehner <julius.dehner@gmail.com>
2019-12-03 23:46:13 +0100

    Vim: Update Coc for python

--- .config/fish/functions/cocrc.fish
+++ .config/fish/functions/cocrc.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function cocrc --description 'alias cocrc nvim ~/.vim/nvim/coc-settings.json'
-	nvim ~/.vim/nvim/coc-settings.json $argv;
+function cocrc --description 'alias cocrc vim ~/.config/nvim/coc-settings.json'
+	vim ~/.config/nvim/coc-settings.json $argv;
 end
--- .emacs.d/customfile.el
+++ .emacs.d/customfile.el
@@ -3,14 +3,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (doom-molokai)))
+ '(custom-enabled-themes '(doom-molokai))
  '(custom-safe-themes
-   (quote
-    ("3a3de615f80a0e8706208f0a71bbcc7cc3816988f971b6d237223b6731f91605" "9b35c097a5025d5da1c97dba45fed027e4fb92faecbd2f89c2a79d2d80975181" "dcf7154867ba67b250fe2c5cdc15a7d170acd9cbe6707cc36d9dd1462282224d")))
+   '("3a3de615f80a0e8706208f0a71bbcc7cc3816988f971b6d237223b6731f91605" "9b35c097a5025d5da1c97dba45fed027e4fb92faecbd2f89c2a79d2d80975181" "dcf7154867ba67b250fe2c5cdc15a7d170acd9cbe6707cc36d9dd1462282224d"))
  '(inhibit-startup-screen t)
  '(org-agenda-files
-   (quote
-    ("~/.emacs.d/config.org" "~/Notebooks/org/gcal.org" "~/Notebooks/org/i.org")))
+   '("~/.emacs.d/config.org" "~/Notebooks/org/gcal.org" "~/Notebooks/org/i.org"))
  '(package-selected-packages
-   (quote
-    (doom-themes command-log-mode golden-ratio-scroll-screen use-package-chords yaml-mode wrap-region iedit expand-region exwm-edit dumb-jump exwm-systemtray exwm xref-js2 js2-refactor js2-mode emmet-mode polymode aggressive-indent agressive-indent atomic-chrome counsel-projectile ace-window ob-kotlin kotlin-mode flatland-theme projectile yasnippet htmlize vterm org-gcal spaceline rainbow-delimiters sudo-edit hungry-delete rainbow-mode rg fzf memoize diff-hl org-bullets which-key magit deferred company adoc-mode dashboard all-the-icons page-break-lines ranger))))
+   '(virtualenvwrapper elpy beacon git-gutter undohist dot-mode ox-reveal highlight-numbers doom-modeline magic-latex-buffer pdf-tools auctex-latexmk company-auctex auctex treemacs-projectile treemacs doom-themes command-log-mode golden-ratio-scroll-screen use-package-chords yaml-mode wrap-region iedit expand-region exwm-edit dumb-jump exwm-systemtray exwm xref-js2 js2-refactor js2-mode emmet-mode polymode aggressive-indent agressive-indent atomic-chrome counsel-projectile ace-window ob-kotlin kotlin-mode flatland-theme projectile yasnippet htmlize vterm org-gcal spaceline rainbow-delimiters sudo-edit hungry-delete rainbow-mode rg fzf memoize diff-hl org-bullets which-key magit deferred company adoc-mode dashboard all-the-icons page-break-lines ranger)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-document-title ((t (:inherit default :weight bold :font "DejaVu Sans Mono" :height 2.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold))))
+ '(org-level-6 ((t (:inherit default :weight bold))))
+ '(org-level-7 ((t (:inherit default :weight bold))))
+ '(org-level-8 ((t (:inherit default :weight bold)))))
