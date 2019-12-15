217be89466e9a2f92a2c2cfec2b50c52d8e04da9
Julius Dehner <julius.dehner@gmail.com>
2019-12-15 21:57:04 +0100

    force blocklists

--- deployment/chroot-install.sh
+++ deployment/chroot-install.sh
@@ -43,7 +43,7 @@ mkinitcpio -p linux
 # install bootloader
 echo 'Installing bootloader'
 pacman -S grub --noconfirm
-grub-install --target=i386-pc ${partition}
+grub-install --target=i386-pc --force ${partition}
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # install networkmanager
@@ -53,6 +53,10 @@ systemctl enable NetworkManager
 # install Xorg
 echo 'Installing Xorg'
 pacman -S --noconfirm xorg xorg-xinit xterm
+
+# install git
+echo 'Installing git'
+pacman -S --noconfirm git
 
 # user management
 echo 'Setting up user'
