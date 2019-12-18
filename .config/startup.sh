ec6d6184618d09e8c5ec7e7a72aed518d2cac607
Julius Dehner <julius.dehner@gmail.com>
2019-12-19 00:47:10 +0100

    Magic: show doom diagnostic at startup, install doom stuff

--- .config/startup.sh
+++ .config/startup.sh
@@ -12,5 +12,7 @@ redshift-gtk -l 48.458510:11.832830 -t 6500K:3800K &
 sxhkd &
 VBoxClient-all &
 xmodmap ~/.Xmodmap
+rg --files ~/.doom.d/ | entr ~/.emacs.d/bin/doom refresh
+notify-send (.emacs.d/bin/doom doctor | tail -2 | sed 's/\x1b\[[0-9;]*m//g')
 echo "started them all"
 
