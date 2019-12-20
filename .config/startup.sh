39623ca85d48f86f20aec4e8efb72ffe2217e1c2
Julius Dehner <julius.dehner@gmail.com>
2019-12-20 18:07:27 +0100

    Set keyboard layout at startup

--- .config/startup.sh
+++ .config/startup.sh
@@ -11,6 +11,7 @@ lightscreen -d &
 redshift-gtk -l 48.458510:11.832830 -t 6500K:3800K &
 sxhkd &
 VBoxClient-all &
+setxkbmap -layout us -variant altgr-intl
 xmodmap ~/.Xmodmap
 # rg --files ~/.doom.d/ | entr ~/.emacs.d/bin/doom refresh
 # notify-send (.emacs.d/bin/doom doctor | tail -2 | sed 's/\x1b\[[0-9;]*m//g')
