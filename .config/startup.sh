7cd41114e2cc3789a9ab783b18fedd1d43f1f092
Julius Dehner <julius.dehner@gmail.com>
2019-12-26 15:35:52 +0100

    Remove redshift from autostart

--- .config/startup.sh
+++ .config/startup.sh
@@ -8,7 +8,6 @@ xrdb ~/.Xresources &
 dunst &
 feh --randomize --bg-fill --no-xinerama ~/Pictures/* &
 lightscreen -d &
-redshift-gtk -l 48.458510:11.832830 -t 6500K:3800K &
 sxhkd &
 VBoxClient-all &
 setxkbmap -layout us -variant altgr-intl
