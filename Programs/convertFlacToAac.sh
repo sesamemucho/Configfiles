73105f53b5f762e50f6465d926fb62094b8816a7
Julius Dehner <julius.dehner@gmail.com>
2020-01-12 06:12:21 +0100

    update aac script

--- Programs/convertFlacToAac.sh
+++ Programs/convertFlacToAac.sh
@@ -2,7 +2,7 @@
 
 for currentFile in "$@"
 do
-	ffmpeg -i $currentFile -vcodec copy -acodec aac -b:a 640k ${currentFile}.temp
-	rm $currentFile
-	mv ${currentFile}.temp $currentFile
+	ffmpeg -i "$currentFile" -vcodec copy -acodec aac -b:a 640k "${currentFile}.temp"
+	rm "$currentFile"
+	mv "${currentFile}.temp" "$currentFile"
 done
