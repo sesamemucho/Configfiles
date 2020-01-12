f5655483383cc99b7dbd265188c814da5de40018
Julius Dehner <julius.dehner@gmail.com>
2020-01-12 08:10:51 +0100

    fix flac extraction script

--- Programs/convertFlacToAac.sh
+++ Programs/convertFlacToAac.sh
@@ -2,7 +2,7 @@
 #TODO output file must end in mkv
 for currentFile in "$@"
 do
-	ffmpeg -i "$currentFile" -vcodec copy -acodec aac -b:a 640k "${currentFile}.temp"
+	ffmpeg -i "$currentFile" -vcodec copy -acodec aac -b:a 640k "${currentFile}.temp.mkv"
 	rm "$currentFile"
-	mv "${currentFile}.temp" "$currentFile"
+	mv "${currentFile}.temp.mkv" "$currentFile"
 done
