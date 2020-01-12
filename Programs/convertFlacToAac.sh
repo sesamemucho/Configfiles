7a235b29c78e1a0706499d9aa746113650117a45
Julius Dehner <julius.dehner@gmail.com>
2020-01-12 06:09:23 +0100

    Add flac to aac script

---
+++ Programs/convertFlacToAac.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for currentFile in "$@"
+do
+	ffmpeg -i $currentFile -vcodec copy -acodec aac -b:a 640k ${currentFile}.temp
+	rm $currentFile
+	mv ${currentFile}.temp $currentFile
+done
