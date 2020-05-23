2b29c5489e831a914104410a82e48ad8165cf30f
Julius Dehner <julius.dehner@gmail.com>
2020-05-23 13:48:43 +0200

    Update gpu-usage script for amd cards

--- Programs/gpu-usage.sh
+++ Programs/gpu-usage.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-gpustat | tail -1 | awk -F '|' '{print "Temp, Utilization: "$2; print "Memory:"$3}' | xargs -0 notify-send
+gpustat | tail -1 | awk -F '|' '{print "Temp, Utilization: "$2; print "Memory:"$3}' | xargs -0 notify-send || \
+sudo radeontop -d - -l 1 | tail -1 | awk -F ',' '{print "Usage:"$2; print "VRAM:"$13 }' | xargs -0 notify-send
