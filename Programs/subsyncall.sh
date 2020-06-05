0333e2bd9af57437d7909fb25857ae3047b16541
Julius Dehner <julius.dehner@gmail.com>
2020-06-05 13:08:06 +0200

    improve subsync script further

--- Programs/subsyncall.sh
+++ Programs/subsyncall.sh
@@ -5,6 +5,6 @@ extension=$1
 shift
 for i in "$@"
 do
-	episode=$(echo $i|grep -oP '.*E..')
+	episode=$(echo $i|grep -oP '.*E\d\d')
 	subsync "$i" -i "$episode.jpn.$extension" -o "$episode.jpn.$extension"
 done
