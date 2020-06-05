cefa9cde8be6f5c448720d06341855537a923962
Julius Dehner <julius.dehner@gmail.com>
2020-06-05 12:16:44 +0200

    Improve subsync script

--- Programs/subsyncall.sh
+++ Programs/subsyncall.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+set -euo pipefail
 
+extension=$1
+shift
 for i in "$@"
 do
 	episode=$(echo $i|grep -oP '.*S..E..')
-	subsync "$i" -i "$episode.jpn.srt" -o "$episode.jpn.srt"
+	subsync "$i" -i "$episode.jpn.$extension" -o "$episode.jpn.$extension"
 done
