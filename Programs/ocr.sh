dbc9d0a78aa2e32e83df9c255b41d12c94e9074e
Julius Dehner <julius.dehner@gmail.com>
2020-06-12 14:52:24 +0200

    improve ocr script

--- Programs/ocr.sh
+++ Programs/ocr.sh
@@ -7,6 +7,26 @@ output=~/Documents/ocr
 sleep 0.3
 maim --select ${image}
 mogrify -modulate 100,0 -resize 400% ${image}
-convert ${image} -channel RGB -negate ${image}
+invert=false
 
-tesseract -l jpn --psm 10 ${image} ${output} && cat ${output}.txt | sed 's/ //g' | tr -d '\n' | xclip -selection clipboard
+while getopts ":i" opt; do
+  case $opt in
+    i)
+      invert=true
+      ;;
+  esac
+done
+
+if [ "$invert" = true ]; then
+  convert ${image} -channel RGB -negate ${image}
+else
+  convert ${image} -channel RGB ${image}
+fi
+
+text=$(tesseract -l jpn --psm 13 ${image} ${output} && cat ${output}.txt | sed 's/ //g' | tr -d '\n' | sed 's/.$//')
+echo $text | xclip -selection clipboard
+translated_text=$(trans -brief ja:en $text)
+pronounciation=$(echo $text | iconv -f utf8 -t shift-jis | kakasi -JH -KH -Ea -s | iconv -f shift-jis -t utf8)
+notify-send "$text
+$pronounciation
+$translated_text"
