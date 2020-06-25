6ca3182bf1d8d988a6bb75869b3c2a6cb3766954
Julius Dehner <julius.dehner@gmail.com>
2020-06-25 18:33:52 +0200

    updates

--- .config/xkeysnail/config.py
+++ .config/xkeysnail/config.py
@@ -34,6 +34,7 @@ define_keymap(re.compile("Brave-browser|Firefox|Google-chrome"), {
     K("C-Shift-w"): K("C-w"),
     # Type C-j to focus to the content
     K("C-j"): K("C-f6"),
+    K("C-Shift-c"): K("C-c"),
     # very naive "Edit in editor" feature (just an example)
     K("C-o"): [K("C-a"), K("C-c"), launch(["gedit"]), sleep(0.5), K("C-v")]
 }, "Firefox and Chrome")
