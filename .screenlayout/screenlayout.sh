c0291e1aeb6bc400b786800d4e56beb9d3fc199e
Julius Dehner <julius.dehner@gmail.com>
2020-09-02 21:15:34 +0200

    Updates

--- .screenlayout/screenlayout.sh
+++ .screenlayout/screenlayout.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
-xrandr --dpi 144 \
-	--output DVI-D-0 --pos 0x570 --mode 1920x1080 --scale 1.5x1.5 \
-	--output DP-0 --pos 2880x0 --primary --mode 3840x2160 \
-	--output DP-4 --pos 6720x570 --mode 1280x1024 --scale 1.5x1.5 \
+xrandr \
+	--output DVI-D-0 --pos 0x0 --mode 1920x1080 \
+	--output DP-4 --pos 1920x56 --mode 1280x1024 \
 	--output HDMI-0 --off
-bspc desktop 1 --to-monitor DP-0
-bspc desktop 2 --to-monitor DP-0
-bspc desktop 3 --to-monitor DVI-D-0
-bspc desktop 4 --to-monitor DVI-D-0
-bspc desktop 5 --to-monitor DP-4
-bspc desktop 6 --to-monitor DP-4
-bspc monitor DP-0 -d 1 2
-bspc monitor DVI-D-0 -d 3 4
-bspc monitor DP-4 -d 5 6
+bspc monitor DVI-D-0 -d 1 2 3
+bspc monitor DP-4 -d 4 5 6
 bspc wm -r
 pkill mpg123
