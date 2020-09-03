611ffcf6d11e0b413486e54fed9f1c403c6dbe93
Julius Dehner <julius.dehner@gmail.com>
2020-09-03 20:21:35 +0200

    Updates

--- Programs/hosts.sh
+++ Programs/hosts.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-curl https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/porn/hosts > /etc/hosts
+echo "0.0.0.0 freshrss.plexiglas.xyz" > /etc/hosts
+echo "0.0.0.0 https://freshrss.plexiglas.xyz" >> /etc/hosts
+echo "0.0.0.0 reddit.com" >> /etc/hosts
+echo "0.0.0.0 old.reddit.com" >> /etc/hosts
+
+
+curl https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts >> /etc/hosts
